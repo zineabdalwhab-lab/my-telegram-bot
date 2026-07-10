5aa2201c6f811f0694c810631c58805568e1cc99
zineabdalwhab-lab <zineabdalwhab@gmail.com>
2026-07-10 10:58:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events
 import os
 
-# ضع بياناتك التي استخرجناها من my.telegram.org
+# بياناتك التي استخرجناها من my.telegram.org
 API_ID = '30653012'
 API_HASH = '988bc3a735d603cc4388a776777a66d4'
 BOT_TOKEN = '8756704689:AAFEIEkwl1_4e9_uHecsyGsop4ZVunDzm0o'
