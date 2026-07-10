1ab8db6f1d78ca4599e1cb3014489653b979117e
zineabdalwhab-lab <zineabdalwhab@gmail.com>
2026-07-10 10:55:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import telebot
+from telethon import TelegramClient, events
 import os
 
-# ضع هنا التوكن الخاص بك بدلاً من هذه الجملة
-API_TOKEN = '8756704689:AAFEIEkwll_4e9_uHecsyGsop4ZVunDzm0o'
+# ضع بياناتك التي استخرجناها من my.telegram.org
+API_ID = '30653012'
+API_HASH = '988bc3a735d603cc4388a776777a66d4'
+BOT_TOKEN = '8756704689:AAFEIEkwl1_4e9_uHecsyGsop4ZVunDzm0o'
 
-bot = telebot.TeleBot(API_TOKEN)
+# إنشاء العميل الخاص بالبوت
+client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك! أنا بوت عبد الوهاب الجديد.")
+@client.on(events.NewMessage(pattern='/start'))
+async def start(event):
+    await event.respond('أنا الآن أعمل بمكتبة Telethon وجاهز لمراقبة البث!')
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "لقد استلمت رسالتك!")
-
-bot.infinity_polling()
+print("البوت يعمل الآن...")
+client.run_until_disconnected()
